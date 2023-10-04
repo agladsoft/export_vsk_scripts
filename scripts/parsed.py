208a1e13774c80c973f7e5042282eee983361472
uvens <uventus1992@gmail.com>
2023-10-04 11:42:16 +0300

    skip only arkas and msc

--- scripts/parsed.py
+++ scripts/parsed.py
@@ -5,7 +5,7 @@ import requests
 
 class Parsed:
     def __init__(self):
-        self.url = "http://51.250.21.126:8004"
+        self.url = "http://92.51.45.231:8004"
         self.headers = {
             'Content-Type': 'application/json'
         }
@@ -68,7 +68,7 @@ EXPORT = ['export', 'экспорт']
 class ParsedDf:
     def __init__(self, df):
         self.df = df
-        self.url = "http://51.250.21.126:8004"
+        self.url = "http://92.51.45.231:8004"
         self.headers = {
             'Content-Type': 'application/json'
         }
