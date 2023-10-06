cd8616f6f35e4e75cb0c5414151b37e29797755a
timur <timurka.zavyalov@mail.ru>
2023-10-06 11:43:14 +0300

    add: added dirs for update auto_tracking

--- scripts/flat_export_vsk.py
+++ scripts/flat_export_vsk.py
@@ -79,10 +79,7 @@ class ExportVSK(object):
         """
         df: DataFrame = pd.read_excel(self.input_file_path, dtype={"ИНН (извлечен через excel)": str})
         df = df.dropna(axis=0, how='all')
-        original_columns = list(df.columns)
         df = df.rename(columns=headers_eng)
-        renamed_columns = list(df.columns)
-        df = df.drop(columns=set(original_columns) & set(renamed_columns))
         df = df.applymap(lambda x: x.strip() if isinstance(x, str) else x)
         self.add_new_columns(df)
         self.change_type_and_values(df)
